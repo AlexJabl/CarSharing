402f37f3985c56d021eeec9c7cbf60388a4f35de
alexj <alexj@172.16.62.93>
2018-03-16 09:59:22 +0100

    Kunde Konstruktoren und Getter und Setter

--- carsharing/carsharing/src/main/java/carsharing/ejb/Kunde.java
+++ carsharing/carsharing/src/main/java/carsharing/ejb/Kunde.java
@@ -38,10 +38,84 @@ public class Kunde {
     private String ort;
     @NotNull
     private String land;
+    
+//<editor-fold defaultstate="collapsed" desc="Konstruktoren">
     public Kunde(){}
-    public Kunde(String vorname, String nachname, String strasse, String hausnummer, int postleizahl, String ort, string land ){}
+    public Kunde(String vorname, String nachname, String strasse, String hausnummer, int postleitzahl, String ort, String land ){
+        this.vorname = vorname;
+        this.nachname = nachname;
+        this.strasse = strasse;
+        this.hausnummer = hausnummer;
+        this.postleitzahl = postleitzahl;
+        this.ort = ort;
+        this.land = land;
+    }
+//</editor-fold>
+
+//<editor-fold defaultstate="collapsed" desc="Getter und Setter">
+    public long getId() {
+        return id;
+    }
     
+    public void setId(long id) {
+        this.id = id;
+    }
     
+    public String getVorname() {
+        return vorname;
+    }
     
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
     
+    public String getNachname() {
+        return nachname;
+    }
+    
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+    
+    public String getStrasse() {
+        return strasse;
+    }
+    
+    public void setStrasse(String strasse) {
+        this.strasse = strasse;
+    }
+    
+    public String getHausnummer() {
+        return hausnummer;
+    }
+    
+    public void setHausnummer(String hausnummer) {
+        this.hausnummer = hausnummer;
+    }
+    
+    public int getPostleitzahl() {
+        return postleitzahl;
+    }
+    
+    public void setPostleitzahl(int postleitzahl) {
+        this.postleitzahl = postleitzahl;
+    }
+    
+    public String getOrt() {
+        return ort;
+    }
+    
+    public void setOrt(String ort) {
+        this.ort = ort;
+    }
+    
+    public String getLand() {
+        return land;
+    }
+    
+    public void setLand(String land) {
+        this.land = land;
+    }
+//</editor-fold> 
+
 }
