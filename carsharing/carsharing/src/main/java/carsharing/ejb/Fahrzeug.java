25a9e66e3f8d750e096da697b15b5c9d33e279b2
alexj <alexj@172.16.62.93>
2018-03-16 10:05:22 +0100

    Fahrzeug Kontruktoren und Getter und Setter

--- carsharing/carsharing/src/main/java/carsharing/ejb/Fahrzeug.java
+++ carsharing/carsharing/src/main/java/carsharing/ejb/Fahrzeug.java
@@ -28,8 +28,48 @@ public class Fahrzeug {
     
     @NotNull
     private String baujahr;
-    public Fahrzeug(){}
-    public Fahrzeug(String hersteller, String modell, String baujahr){}
     
+//<editor-fold defaultstate="collapsed" desc="Konstruktoren">
+    public Fahrzeug(){}
+    public Fahrzeug(String hersteller, String modell, String baujahr){
+        this.hersteller = hersteller;
+        this.modell = modell;
+        this.baujahr = baujahr;
+    }
+//</editor-fold>
+
+//<editor-fold defaultstate="collapsed" desc="Getter und Setter">
+    public long getId() {
+        return id;
+    }
+    
+    public void setId(long id) {
+        this.id = id;
+    }
+    
+    public String getHersteller() {
+        return hersteller;
+    }
+    
+    public void setHersteller(String hersteller) {
+        this.hersteller = hersteller;
+    }
+    
+    public String getModell() {
+        return modell;
+    }
+    
+    public void setModell(String modell) {
+        this.modell = modell;
+    }
+    
+    public String getBaujahr() {
+        return baujahr;
+    }
+    
+    public void setBaujahr(String baujahr) {
+        this.baujahr = baujahr;
+    }
+//</editor-fold>
     
 }
